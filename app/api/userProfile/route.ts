5d070dc14f4d30535283caad1c1ee0061f4d2556
nabilmaulanah <nabilmaulanah@gmail.com>
2025-12-13 11:14:38 +0700

    Edit Profile

--- app/api/userProfile/route.ts
+++ app/api/userProfile/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { db } from "../../../lib/db";
+import { db } from "../../../lib/db"; 
 
 export async function GET() {
-  const cookieStore: ReturnType<typeof cookies> = cookies();
+  const cookieStore = await cookies();
   const userId = cookieStore.get("user_id")?.value;
 
   if (!userId) {
@@ -11,11 +11,11 @@ export async function GET() {
   }
 
   const [rows]: any = await db.query(
-    "SELECT email, username, no_tlp, password FROM user WHERE id = ?",
+    "SELECT username, email, no_tlp, password FROM user WHERE id = ?",
     [userId]
   );
 
-  if (rows.length === 0) {
+  if (!rows || rows.length === 0) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
 
