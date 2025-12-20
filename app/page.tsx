60e4fc11fc976a415061f1e31772fdb3a6c0f89a
ilhamhlmi <ilhamhilmi206@gmail.com>
2025-12-20 14:12:49 +0700

    remove container ulasan

--- app/page.tsx
+++ app/page.tsx
@@ -185,7 +185,7 @@ export default function Home() {
       </section>
 
       <section className="w-full flex items-center bg-slate-200">
-        <div className="container mx-auto">
+        <div className="max-w-screen mx-auto">
           <div className="flex justify-center items-center" data-aos="fade-up">
             <h1 className="font-poppins text-3xl 2xl:text-4xl text-darkb border-b pb-5 border-secondary font-semibold text-center max-w-[300px] lg:max-w-none mb-8">
               Kata Mereka Tentang LapCare
